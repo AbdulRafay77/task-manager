5754dddb3a94e0c4620f391bd75470128a166146
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-07-18 22:09:22 +0500

    version 2

--- backend-task-manager/models/UserModel.js
+++ backend-task-manager/models/UserModel.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next){
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
-    next();
+    //next();
 })
 
 // static method to login user
