ac555a2caae175f71f76b335fb97807ddc255809
Abdul Rafay Siddiqui <abdulrafaysiddiquib153@gmail.com>
2026-07-18 20:53:46 +0500

    version 2

--- backend-task-manager/models/UserModel.js
+++ backend-task-manager/models/UserModel.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please enter an password'],
         minlength: [6, 'Minimum password length is 6 characters']
-    },
+    }
 });
 
 // fire a function before doc saved to db
